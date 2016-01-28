ca8fb28e6d30167c014b3f12b0e13369d64aac7f
Tyler Sorenson <Tyler Sorenson>
2016-01-27 21:46:57 -0700

    World

--- Repository Practice/HelloWorld.java
+++ Repository Practice/HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
 	public static void main(String[] args) {
-		System.out.print("Hello GitHub\n");
+		System.out.print("Hello GitHub World\n");
 		System.out.print("Testing...");
 	}
 }
